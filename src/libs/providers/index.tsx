7638204dd8fde59b4f62fdc5a090a2d583256499
R1013-T <takahashi.ryunosuke.job@gmail.com>
2025-07-29 08:57:41 +0900

    init ui

---
+++ src/libs/providers/index.tsx
@@ -0,0 +1,13 @@
+import { type PropsWithChildren } from "react";
+import { RouteProvider } from "./route-provider";
+import { ThemeProvider } from "./theme-provider";
+
+export function Providers({ children }: PropsWithChildren) {
+  return (
+    <RouteProvider>
+      <ThemeProvider>
+        {children}
+      </ThemeProvider>
+    </RouteProvider>
+  );
+}
